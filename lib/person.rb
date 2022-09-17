ed762174ee034a50f9db8b0ee380216a0d447acb
Win Feigle <win.feigle@gmail.com>
2022-09-16 22:44:36 -0600

    commit tree 1aa22076124895563a2283b189a92d65e053ddef

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,80 @@
 # your code goes here
+
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(value)
+        @happiness = if value > 10
+                            10
+                        elsif value < 0
+                            0
+                        else
+                            value
+                        end
+    end
+
+    def hygiene=(value)
+        @hygiene = if value > 10
+                            10
+                        elsif value < 0
+                            0
+                        else
+                            value
+                        end
+    end
+
+    def happy?
+        self.happiness > 7 ? true : false
+    end
+
+    def clean?
+        self.hygiene > 7 ? true : false
+    end
+
+    def get_paid(amount)
+        @bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        # calls on the #hygiene= method to increment hygiene
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        # calls on the #happiness= method to increment happiness
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        friend.happiness += 3
+        self.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == 'politics'
+            friend.happiness -= 2
+            self.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == 'weather'
+            friend.happiness += 1
+            self.happiness += 1
+            "blah blah sun blah rain"
+        else 
+            "blah blah blah blah blah"
+        end
+    end
+
+end
